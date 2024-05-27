b84700ce4bfb8d4d50d73c4f5aed79a1a51021d8
Divya <divyapandey090901@gmail.com>
2024-05-27 21:46:17 +0530

    first file commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link} from 'react-router-dom';
 
 const Navbar = () => {
+  const[menu,setMenu]=useState('Shop')
   return (
-    <div className='navbar'>
+    <div className='navbar'> 
       <div className='nav-logo'>
         <img src={logo} alt="" />
         <p>Shoppee</p>
       </div>
       <ul className='nav-menu'>
-        <li className='menu'>Shop <hr /></li>
-        <li className='menu'>Men</li>
-        <li className='menu'>Women</li>
-        <li className='menu'>Kids</li>
+        <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu===("shop")?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}} to='/mens'>Men</Link>{menu===("mens")?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration:'none'}} to='/womens'>Women</Link>{menu===("womens")?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration:'none'}} to='/kids'>Kid</Link>{menu===("kids")?<hr/>:<></>}</li>
       </ul>
       <div className='nav-login-cart'>
-        <button className='nav-login'> Login </button>
-        <img src={cart_icon} alt="" />
+       <Link to='/login'> <button> Login </button></Link> 
+       <Link to='/cart'> <img src={cart_icon} alt="" /></Link>
       <div className='nav-cart-count'>0</div>
       </div>
     </div>
